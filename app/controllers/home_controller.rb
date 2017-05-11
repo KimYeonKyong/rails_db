49fb9b9ab0d99775461db70b0d364d6533890d89
rud9138@likelion.org <rud9138@likelion.org>
2017-05-11 20:56:47 +0900

    html update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+  	@qall = Question.all
   end
+
+  def new
+  end
+
+  def create
+  	q = Question.new
+  	q.name = params[:db_name]
+  	q.content = params[:db_content]
+  	q.save
+
+  	Question.Create!(name:[])
+  end
+
+
 end
